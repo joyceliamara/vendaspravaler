6a6ba8b6856287328babc882a3d62d9dd73a7842
joycemarinheiro <joycemarinheiro.jobs@gmail.com>
2025-06-03 18:39:09 -0300

    feat: add google tag manager suport

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,9 @@
 import type React from "react";
 import type { Metadata } from "next/dist/lib/metadata/types/metadata-interface";
-import { Poppins, Inter } from "next/font/google";
 import "./globals.css";
 import { ThemeProvider } from "@/components/theme-provider";
 import { Ubuntu } from 'next/font/google';
+import Script from 'next/script';
 
 const ubuntu = Ubuntu({
   subsets: ["latin"],
@@ -42,6 +42,8 @@ export const metadata: Metadata = {
   },
 };
 
+const GTM_ID = 'GTM-558RLVX3';
+
 export default function RootLayout({
   children,
 }: Readonly<{
@@ -50,8 +52,34 @@ export default function RootLayout({
   return (
     <html lang="pt-BR" suppressHydrationWarning>
       <head>
+        {GTM_ID && (
+          <Script
+            id="google-tag-manager-script"
+            strategy="afterInteractive"
+            dangerouslySetInnerHTML={{
+              __html: `
+                (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+                new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+                j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+                'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+                })(window,document,'script','dataLayer','${GTM_ID}');
+              `,
+            }}
+          />
+        )}
       </head>
       <body className={`${ubuntu.variable} bg-background`}>
+        {GTM_ID && (
+          <noscript>
+            <iframe
+              src={`https://www.googletagmanager.com/ns.html?id=${GTM_ID}`}
+              height="0"
+              width="0"
+              style={{ display: 'none', visibility: 'hidden' }}
+            ></iframe>
+          </noscript>
+        )}
+
         <ThemeProvider
           attribute="class"
           defaultTheme="dark"
